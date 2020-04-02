b1184f8cbefd83b65323f9c9fca091212c87c87f
NicoMosty <40640296+NicoMosty@users.noreply.github.com>
2020-04-02 16:12:30 -0500

    MI

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,11 @@ var i=1;
 
 function menuhide(){
     var VERIF=document.getElementById("menu-js").style.display;
-    if (VERIF=="block"){
-        document.getElementById("menu-js").style.display = "none";
+    if (VERIF=="none"){
+        document.getElementById("menu-js").style.display = "block";
         // document.getElementsByClassName("div#main-js.info1").style.marginTop = "100px";
     } else{
-        document.getElementById("menu-js").style.display = "block";
+        document.getElementById("menu-js").style.display = "none";
         // document.getElementsByClassName("div#main-js.info1").style.marginTop = "-100px";    
     }
 }
