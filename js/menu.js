5fc37891e4638f185f1ab7df094a68144c7d99f3
NicoMosty <40640296+NicoMosty@users.noreply.github.com>
2020-04-02 19:23:36 -0500

    Change None in menu.js

--- js/menu.js
+++ js/menu.js
@@ -2,11 +2,11 @@ var i=1;
 
 function menuhide(){
     var VERIF=document.getElementById("menu-js").style.display;
-    if (VERIF=="none"){
-        document.getElementById("menu-js").style.display = "block";
+    if (VERIF=="block"){
+        document.getElementById("menu-js").style.display = "none";
         // document.getElementsByClassName("div#main-js.info1").style.marginTop = "100px";
     } else{
-        document.getElementById("menu-js").style.display = "none";
+        document.getElementById("menu-js").style.display = "block";
         // document.getElementsByClassName("div#main-js.info1").style.marginTop = "-100px";    
     }
 }
