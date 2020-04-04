a945cc4061d017a7397fdca0c033798fe93246b9
NicoMosty <40640296+NicoMosty@users.noreply.github.com>
2020-04-04 13:33:20 -0500

    Adding Touch in Banner v2.0

--- js/banner.js
+++ js/banner.js
@@ -20,13 +20,18 @@ function openbanner(){
 }
 
 // Funcion ONTOUCHMOVE
+var x_o; var y_o;
 function touch_start_banner(){
-    var x_o = event.touches[0].clientX;
-    var y_o = event.touches[0].clientY;
+    x_o = event.touches[0].clientX;
+    y_o = event.touches[0].clientY;
+    console.clear();
+    console.log('x_o=',x_o,'y_o=',y_o);
 }
 function touch_end_banner(){
-    var x_f = event.touches[0].clientX;
-    var y_f = event.touches[0].clientY;
+    var x_f = event.changedTouches[0].clientX;
+    var y_f = event.changedTouches[0].clientY;
+    console.log('--------------------');
+    console.log('x_f=',x_f,'y_f=',y_f);
     if ((Math.abs((y_f-y_o)/y_o))<0.05){
         if ((x_f-x_o)<0){
             document.getElementById("change-banner-1").src=imag[i].src;
