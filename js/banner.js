5121eda674e9102ebce6d26dfa55159b9d295052
NicoMosty <40640296+NicoMosty@users.noreply.github.com>
2020-04-04 13:21:18 -0500

    Adding Touch Functions

--- js/banner.js
+++ js/banner.js
@@ -19,7 +19,37 @@ function openbanner(){
     timer=setTimeout(function(){ openbanner(); },time);
 }
 
-
+// Funcion ONTOUCHMOVE
+function touch_start_banner(){
+    var x_o = event.touches[0].clientX;
+    var y_o = event.touches[0].clientY;
+}
+function touch_end_banner(){
+    var x_f = event.touches[0].clientX;
+    var y_f = event.touches[0].clientY;
+    if ((Math.abs((y_f-y_o)/y_o))<0.05){
+        if ((x_f-x_o)<0){
+            document.getElementById("change-banner-1").src=imag[i].src;
+            i++;
+            if(i==imag.length){
+                i=0;
+            }
+            console.log(i);
+            clearTimeout(timer);
+            timer=setTimeout(function(){ openbanner(); },time);
+        } else{
+            document.getElementById("change-banner-1").src=imag[i].src;
+            if(i==0){
+                i=imag.length-1;
+            } else{
+                i--;
+            }
+            console.log(i);
+            clearTimeout(timer);
+            timer=setTimeout(function(){ openbanner(); },time);
+        }
+    }
+}
 
 function next(){
     document.getElementById("change-banner-1").src=imag[i].src;
