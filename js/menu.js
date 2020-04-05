870a1620280af350bd22f2b956224040a21f043f
NicoMosty <40640296+NicoMosty@users.noreply.github.com>
2020-04-04 23:01:38 -0500

    AAA

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,6 @@
 var trans=100;
 function menuhide(){
     var VERIF=document.getElementById("menu-js");
-    VERIF.style.transform= "translateY(-"+trans.toString()+"px)";
-    VERIF.style.marginBottom="-"+trans.toString()+"px";
-    VERIF.style.transition = "2s";
-}
-
-function menuhide(){
-    var VERIF=document.getElementById("menu-js");
     if (VERIF.style.transform=="translateY(0px)" || VERIF.style.transform==""){
         VERIF.style.transform= "translateY(-"+trans.toString()+"px)";
         VERIF.style.marginBottom="-"+trans.toString()+"px";
