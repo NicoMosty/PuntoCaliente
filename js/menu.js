635587e2dae2b7abe2e297bf82a20334ae2b9176
NicoMosty <40640296+NicoMosty@users.noreply.github.com>
2020-04-06 02:12:44 -0500

    !important in css for hide menu (FINAL)

--- js/menu.js
+++ js/menu.js
@@ -1,29 +1,31 @@
 var trans=100;
+var time=1;
+var VERIF=document.getElementById("menu-js");
+var CHECK=window.innerWidth;
+
 function menuhide(){
-    var VERIF=document.getElementById("menu-js");
     if (VERIF.style.transform=="translateY(0px)"){
         VERIF.style.transform= "translateY(-"+trans.toString()+"px)";
         VERIF.style.marginBottom="-"+trans.toString()+"px";
-        VERIF.style.transition = "1s";
+        VERIF.style.transition = time.toString()+"s";
     } 
     else {   
         VERIF.style.transform= "translateY(0px)";
         VERIF.style.marginBottom="0px";
-        VERIF.style.transition = "1s";
+        VERIF.style.transition = time.toString()+"s";
+        VERIF.style.transition = time.toString()+"s";
     }
 }
 
 function cambiotamaño(){
-    var CHECK=window.innerWidth;
-    var VERIF=document.getElementById("menu-js");
     if(CHECK<900){
-        VERIF.style.transform= "translateY(-"+trans.toString()+"px)";
-        VERIF.style.marginBottom="-"+trans.toString()+"px";
+        VERIF.style.transform= "translateY(-100px)";
+        VERIF.style.marginBottom="-100px";
         VERIF.style.transition = "1s";
     } 
     if(CHECK>900){
         VERIF.style.transform= "translateY(0px)";
         VERIF.style.marginBottom="0px";
-        VERIF.style.transition = "1s";      
+        VERIF.style.transition = time.toString()+"s";      
     }
 }
