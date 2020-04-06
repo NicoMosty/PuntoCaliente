4b3352df0c533efe59578ce4f7e8d0a6bd841723
NicoMosty <40640296+NicoMosty@users.noreply.github.com>
2020-04-06 02:33:04 -0500

    FINAL CHANGES + Resize window

--- js/menu.js
+++ js/menu.js
@@ -5,27 +5,19 @@ var CHECK=window.innerWidth;
 
 function menuhide(){
     if (VERIF.style.transform=="translateY(0px)"){
-        VERIF.style.transform= "translateY(-"+trans.toString()+"px)";
-        VERIF.style.marginBottom="-"+trans.toString()+"px";
+        VERIF.style.transform= "translateY(-"+trans.toString()+"px) ";
+        VERIF.style.marginBottom="-"+trans.toString()+"px ";
         VERIF.style.transition = time.toString()+"s";
     } 
     else {   
-        VERIF.style.transform= "translateY(0px)";
-        VERIF.style.marginBottom="0px";
+        VERIF.style.transform= "translateY(0px) ";
+        VERIF.style.marginBottom="0px ";
         VERIF.style.transition = time.toString()+"s";
         VERIF.style.transition = time.toString()+"s";
     }
 }
 
+var VERIF1=document.getElementsByClassName("menu");
 function cambiotamaño(){
-    if(CHECK<900){
-        VERIF.style.transform= "translateY(-100px)";
-        VERIF.style.marginBottom="-100px";
-        VERIF.style.transition = "1s";
-    } 
-    if(CHECK>900){
-        VERIF.style.transform= "translateY(0px)";
-        VERIF.style.marginBottom="0px";
-        VERIF.style.transition = time.toString()+"s";      
-    }
+    location.reload();
 }
